3936348a511da2a9ea73b114a4ae0be1bde6e4ac
Pete Davis <oldmanplanet@gmail.com>
2018-08-11 18:57:17 +0100

    babel transpilation

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,12 @@
+import babel from "rollup-plugin-babel";
+
 export default {
   input: "src/main.js",
+  plugins: [
+    babel({
+      exclude: "node_modules/**"
+    })
+  ],
   output: {
     format: "iife",
     sourcemap: true,

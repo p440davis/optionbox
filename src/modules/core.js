72adcf557217b6e0abc96938b86e97b445f39f13
Pete Davis <pete@parttimecarnivore.org>
2018-08-24 16:29:22 +0100

    Initial preselecting code

--- src/modules/core.js
+++ src/modules/core.js
@@ -1,95 +1,108 @@
 const core = {
-  init(selector) {
-    let elements = selector
-      ? document.querySelectorAll(selector)
-      : document.querySelectorAll("select");
-    let selects = [];
-    if (elements.length) {
-      [].forEach.call(elements, element => {
-        if (element.tagName == "SELECT") {
-          selects.push(element);
-        } else
-          console.warn(
-            "Optionbox warning: Your selection includes a <" +
-              element.tagName.toLowerCase() +
-              ">. Only <select> elements can be converted to an optionbox"
-          );
-      });
-      this.create(selects);
-      this.listen(document.querySelectorAll(".optionbox-radio"));
-    }
-  },
+    init(selector) {
+        let elements = selector
+            ? document.querySelectorAll(selector)
+            : document.querySelectorAll("select");
+        let selects = [];
+        if (elements.length) {
+            [].forEach.call(elements, element => {
+                if (element.tagName == "SELECT") {
+                    selects.push(element);
+                } else
+                    console.warn(
+                        "Optionbox warning: Your selection includes a <" +
+                            element.tagName.toLowerCase() +
+                            ">. Only <select> elements can be converted to an optionbox"
+                    );
+            });
+            this.create(selects);
+            //this.preselect();
+            this.listen(document.querySelectorAll(".optionbox-radio"));
+        }
+    },
 
-  create(selects) {
-    selects.forEach(select => {
-      let optionsBox = document.createElement("span");
+    create(selects) {
+        selects.forEach(select => {
+            let optionsBox = document.createElement("span");
 
-      optionsBox.classList.add("optionbox");
-      optionsBox.innerHTML = this.replicate(
-        select.querySelectorAll("option"),
-        select.name
-      );
+            optionsBox.classList.add("optionbox");
+            optionsBox.innerHTML = this.replicate(
+                select.querySelectorAll("option"),
+                select.name
+            );
 
-      select.insertAdjacentElement("beforebegin", optionsBox);
+            select.insertAdjacentElement("beforebegin", optionsBox);
 
-      select.selectedIndex = -1;
-      select.hidden = true;
-    });
-  },
+            select.selectedIndex = -1;
+            //select.hidden = true;
+        });
+    },
 
-  replicate(options, name) {
-    let content = "";
+    replicate(options, name) {
+        let content = "";
 
-    [].forEach.call(options, option => {
-      content += `
+        [].forEach.call(options, option => {
+            content += `
         <label class="optionbox-item">
           <input
             type="radio"
             class="optionbox-radio"
             name="${name}-options"
             data-optionbox-name="${name}"
-            data-optionbox-value="${option.value}">
+            data-optionbox-value="${option.value}"
+            checked="${option.hasAttribute("selected")}">
           ${option.innerHTML}
         </label>`;
-    });
+        });
 
-    return content;
-  },
+        return content;
+    },
 
-  listen(radios) {
-    [].forEach.call(radios, radio => {
-      radio.onclick = () => {
-        let name = radio.dataset.optionboxName;
-        let value = radio.dataset.optionboxValue;
-        this.select(radio, name, value);
-      };
-    });
-  },
+    preselect() {
+        let radios = document.querySelectorAll(
+            ".optionbox-item [checked=true]"
+        );
+        [].forEach.call(radios, radio => {
+            let name = radio.dataset.optionboxName;
+            let value = radio.dataset.optionboxValue;
+            this.select(radio, name, value);
+        });
+    },
 
-  select(radio, name, value) {
-    let select = document.querySelector(`[name=${name}]`);
-    let optionItem = radio.closest(".optionbox-item");
-    let optionsBox = radio.closest(".optionbox");
-    let optionSiblings = optionsBox.querySelectorAll(
-      ".optionbox-radio:not(:checked)"
-    );
+    listen(radios) {
+        [].forEach.call(radios, radio => {
+            radio.onclick = () => {
+                let name = radio.dataset.optionboxName;
+                let value = radio.dataset.optionboxValue;
+                this.select(radio, name, value);
+            };
+        });
+    },
 
-    if (select.value != value) {
-      // select if not previously selected
-      select.value = value;
-      optionItem.classList.add("selected");
-      optionsBox.classList.add("closed");
-      [].forEach.call(optionSiblings, sibling =>
-        sibling.closest("label").classList.remove("selected")
-      );
-    } else {
-      // deselect if previously selected
-      select.selectedIndex = -1;
-      radio.checked = false;
-      optionItem.classList.remove("selected");
-      optionsBox.classList.remove("closed");
+    select(radio, name, value) {
+        let select = document.querySelector(`[name=${name}]`);
+        let optionItem = radio.closest(".optionbox-item");
+        let optionsBox = radio.closest(".optionbox");
+        let optionSiblings = optionsBox.querySelectorAll(
+            ".optionbox-radio:not(:checked)"
+        );
+
+        if (select.value != value) {
+            // select if not previously selected
+            select.value = value;
+            optionItem.classList.add("selected");
+            optionsBox.classList.add("closed");
+            [].forEach.call(optionSiblings, sibling =>
+                sibling.closest("label").classList.remove("selected")
+            );
+        } else {
+            // deselect if previously selected
+            select.selectedIndex = -1;
+            radio.checked = false;
+            optionItem.classList.remove("selected");
+            optionsBox.classList.remove("closed");
+        }
     }
-  }
 };
 
 export { core };

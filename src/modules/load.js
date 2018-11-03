870bf923a9fda2d2a6f8151cc0a210d246ddddd5
Pete Davis <pete@parttimecarnivore.org>
2018-11-03 17:04:59 +0000

    Persist selecte value + split core js files

---
+++ src/modules/load.js
@@ -0,0 +1,84 @@
+import { update } from "./update";
+import { persist } from "./persist";
+
+const load = {
+    init(selector) {
+        let elements = selector
+            ? document.querySelectorAll(selector)
+            : document.querySelectorAll("select");
+        let selects = [];
+        if (elements.length) {
+            [].forEach.call(elements, element => {
+                if (element.tagName == "SELECT") {
+                    selects.push(element);
+                } else
+                    console.warn(
+                        "Optionbox warning: Your selection includes a <" +
+                        element.tagName.toLowerCase() +
+                        ">. Only <select> elements can be converted to an optionbox"
+                    );
+            });
+            this.create(selects);
+            update.listen(document.querySelectorAll(".optionbox-radio"));
+            this.clickAllSelected();
+        }
+    },
+
+    create(selects) {
+        selects.forEach(select => {
+            let optionsBox = document.createElement("span");
+            let storedValue = persist.get(select.name);
+
+            optionsBox.classList.add("optionbox");
+            optionsBox.innerHTML = this.replicate(
+                select.querySelectorAll("option"),
+                select.name,
+                storedValue
+            );
+
+            select.insertAdjacentElement("beforebegin", optionsBox);
+
+            select.selectedIndex = -1;
+            select.hidden = true;
+        });
+    },
+
+    replicate(options, name, storedValue) {
+        let content = "";
+
+        [].forEach.call(options, option => {
+            let selected = false;
+            if (storedValue === option.value) {
+                selected = true;
+            } else if (!storedValue && option.hasAttribute("selected")) {
+                selected = true;
+            }
+
+            content += `
+                <label class="optionbox-item">
+                <input
+                    type="radio"
+                    class="optionbox-radio"
+                    name="${name}-options"
+                    data-optionbox-name="${name}"
+                    data-optionbox-value="${option.value}"
+                    checked="${selected}">
+                ${option.innerHTML}
+                </label>`;
+        });
+
+        return content;
+    },
+
+    clickAllSelected() {
+        let checked = document.querySelectorAll(
+            ".optionbox-radio[checked=true]"
+        );
+
+        [].forEach.call(checked, radio => {
+            radio.click();
+        });
+    }
+};
+
+export { load };
